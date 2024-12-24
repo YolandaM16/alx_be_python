61bce44001f4eed7b196d35e8f066bc322d4ef9a
YolandaM16 <mookie.mokwena@gmail.com>
2024-12-24 09:41:35 +0200

    Daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,33 +1,14 @@
 <<<<<<< HEAD
-# Step 1: Prompt for a Single Task
-task = input("Enter the task description: ")
-priority = input("Enter the task's priority (high, medium, low): ").lower()
-time_bound = input("Is the task time-sensitive? (yes or no) ").lower()
-=======
-#step 1: Prompt for a Single Task
-task = input("Enter the task description: ")
-priority = input("Enter the task's priority (high, medium, low): ").lower()
-time_bound = input("Is the task time-sensitive? (yes or no): ").lower()
->>>>>>> c2155adfbe21c9fc880b0bade5719b037d4d801d
-
-# Step 2: Process the Task Based on Priority and Time Sensitivity
-match priority:
-    case "high":
-        message = "This is a high-priority task."
-    case "medium":
-        message = "This is a medium-priority task."
-    case "low":
-<<<<<<< HEAD
         message = "This is a low-priority task."
 =======
         message = "This is low-priority task."
 >>>>>>> c2155adfbe21c9fc880b0bade5719b037d4d801d
+>>>>>>> 6415922f8124fcf5e5c392544de0a85ab0d2c49a
     case _:
         message = "Invalid priority level. Please enter high, medium, or low."
         print(message)
         exit()
 
-# Add time sensitivity details
 if time_bound == "yes":
 <<<<<<< HEAD
     message += " It requires immediate attention today!"
@@ -41,6 +22,8 @@ elif time_bound == "no":
 else:
     print("Invalid input for time sensitivity. Please enter yes or no.")
     exit()
+<<<<<<< HEAD
+=======
 
 # Step 3: Provide a Customized Reminder
 <<<<<<< HEAD
@@ -50,3 +33,4 @@ print(message)
 print(f"Task Remindeer: {task}")
 print(message)
 >>>>>>> c2155adfbe21c9fc880b0bade5719b037d4d801d
+>>>>>>> 6415922f8124fcf5e5c392544de0a85ab0d2c49a
