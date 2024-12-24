edb1d2eae7087d3d093a57f022772b5dd93598ef
Yolanda <mookie.mokwena@gmail.com>
2024-12-23 23:20:08 -0800

    Daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,19 @@
-#step 1: Prompt for a Single Task
-task = input("Enter the task description: ")
+task = input("Enter the tas description: ")
 priority = input("Enter the task's priority (high, medium, low): ").lower()
-time_bound = input("Is the task time-sensitive? (yes or no): ").lower()
+time_bound = input("Is the task time-sensitive? (ye or no): ").lower()
 
-# Step 2: Process the Task Based on Priority and Time Sensitivity
 match priority:
     case "high":
         message = "This is a high-priority task."
     case "medium":
         message = "This is a medium-priority task."
     case "low":
-        message = "This is low-priority task."
+        message = "This is a low-priority task."
     case _:
         message = "Invalid priority level. Please enter high, medium, or low."
         print(message)
         exit()
-
-# Add time sensitivity details
+        
 if time_bound == "yes":
     message += "It requires immediate attention today!"
 elif time_bound == "no":
@@ -24,7 +21,3 @@ elif time_bound == "no":
 else:
     print("Invalid input for time sensitivity. Please enter yes or no.")
     exit()
-
-# Step 3: Provide a Customized Reminder
-print(f"Task Remindeer: {task}")
-print(message)
