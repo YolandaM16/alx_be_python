e5f1861dfdaa1623ceaad7c718c39a1991edd6df
YolandaM16 <mookie.mokwena@gmail.com>
2025-01-23 01:17:40 +0200

    library_system.py

--- oop/main.py
+++ oop/main.py
@@ -1,20 +1,20 @@
-from book_class import Book
+# from book_class import Book
 
-def main():
-    # Creating an instance of Book
-    my_book = Book("1984", "George Orwell", 1949)
+# def main():
+#     # Creating an instance of Book
+#     my_book = Book("1984", "George Orwell", 1949)
 
-    # Demonstrating the __str__ method
-    print(my_book)  # Expected to use __str__
+#     # Demonstrating the __str__ method
+#     print(my_book)  # Expected to use __str__
 
-    # Demonstrating the __repr__ method
-    print(repr(my_book))  # Expected to use __repr__
+#     # Demonstrating the __repr__ method
+#     print(repr(my_book))  # Expected to use __repr__
 
-    # Deleting a book instance to trigger __del__
-    del my_book
+#     # Deleting a book instance to trigger __del__
+#     del my_book
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
 
 from library_system import Book, EBook, PrintBook, Library
